920c474abe9ab992b5ffc7251a629400b116ce79
aditya <aditya.marella@gmail.com>
2014-04-13 20:11:30 -0400

    adding appengine bootstrap code

---
+++ visualization/main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#
+
+import os
+import urllib
+import httplib2
+import logging
+import webapp2
+from webapp2_extras import jinja2
+from webapp2_extras import json
+from django.utils import simplejson
+
+
+############################################################################
+# BaseHandler subclasses RequestHandler so that we can use jinja
+class BaseHandler(webapp2.RequestHandler):
+
+    @webapp2.cached_property
+    def jinja2(self):
+        # Returns a Jinja2 renderer cached in the app registry.
+        return jinja2.get_jinja2(app=self.app)
+
+        # This will call self.response.write using the specified template and context.
+        # The first argument should be a string naming the template file to be used. 
+        # The second argument should be a pointer to an array of context variables
+        #  that can be used for substitutions within the template
+    def render_response(self, _template, **context):
+        values = {'url_for': self.uri_for}
+        logging.info(context)
+        values.update(context)
+        self.response.headers['Content-Type'] = 'text/html'
+
+        # Renders a template and writes the result to the response.
+        try: 
+            rv = self.jinja2.render_template(_template, **values)
+            self.response.headers['Content-Type'] = 'text/html; charset=utf-8'
+            self.response.write(rv)
+        except TemplateNotFound:
+            self.abort(404)
+
+
+# Class MainHandler now subclasses BaseHandler instead of webapp2
+class MainHandler(BaseHandler):
+
+    # This method should return the html to be displayed
+    def get(self):
+        context = {}
+        # and render the response
+        self.render_response('index.html', **context)
+
+app = webapp2.WSGIApplication([('/.*', MainHandler)], debug=True)
