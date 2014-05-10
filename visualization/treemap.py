f926a6b1756627e0e4bfac4ae72d84fcb32b5b07
Adam Durity <adam@durity.net>
2014-05-10 01:09:12 -0400

    Implements treemap visualization.

--- visualization/treemap.py
+++ visualization/treemap.py
@@ -11,4 +11,9 @@ app = Flask(__name__)
 @app.route('/treemap')
 def index():
     context = {}
+    with open('data/category_tree.json') as fp:
+        data = json.load(fp)
+
+    context['data'] = json.dumps(data)
+
     return render_template('treemap.html', **context)
