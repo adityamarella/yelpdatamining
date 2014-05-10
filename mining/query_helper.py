790784b4b4182ba81966bb5a9a6da09177cc1519
Adam Durity <adam@durity.net>
2014-05-10 19:40:19 -0400

    Updates category_tree.json from computed categories for visualization.

--- mining/query_helper.py
+++ mining/query_helper.py
@@ -1,18 +1,20 @@
+from collections import deque
 import json
 import sqlite3
 import sys
 
-class CategoryTree(object):
+class Category(object):
     """
     The following three attributes define this datastructure
     category_id: id of the category given in categories table
     title: category title
-    children: is a list of CategoryTree objects
+    children: is a list of Category objects
     """
 
-    def __init__(self, cid = -1, title=""):
+    def __init__(self, cid = -1, title="", business_count=0):
         self.category_id = cid
         self.title = title
+        self.business_count = business_count
         self.parent = None
         self.children = []
 
@@ -23,11 +25,11 @@ class CategoryTree(object):
             self.title,
         )
 
-class CategoryTreeEncoder(json.JSONEncoder):
-    """A JSONEncoder for CategoryTree.
+class CategoryEncoder(json.JSONEncoder):
+    """A JSONEncoder for Category.
     """
     def default(self, obj):
-        if isinstance(obj, CategoryTree):
+        if isinstance(obj, Category):
             return {'alias': obj.category_id,
                     'title': obj.title,
                     'category': obj.children
@@ -55,7 +57,7 @@ class CategoryTreeCreator(object):
         for row in rows:
             arr = categories.setdefault(row[0], {})
             arr['title'] = row[1]
-            arr['tree'] = CategoryTree(cid=row[0], title=row[1])
+            arr['tree'] = Category(cid=row[0], title=row[1])
             if row[2] != None:
                 arr.setdefault('values', []).append(row[2])
 
@@ -67,7 +69,7 @@ class CategoryTreeCreator(object):
                 subcategories_union.add(cid)
                 child_node = categories[cid]['tree']
                 tree.children.append(child_node)
-        
+
 
         #find the top level nodes of the category hierarchy
         #Top level nodes can be computed by doing a set-difference between
@@ -75,9 +77,9 @@ class CategoryTreeCreator(object):
         categories_union = set(categories.keys())
         top_level_nodes = categories_union.difference(subcategories_union)
 
-        #create a root node and append the top level node to the root node to 
+        #create a root node and append the top level node to the root node to
         #complete the tree construction
-        root = CategoryTree(cid=-1, title="All")
+        root = Category(cid=-1, title="All")
         for cid in top_level_nodes:
             root.children.append(categories[cid]['tree'])
 
@@ -87,11 +89,14 @@ class CategoryTreeCreator(object):
         #remove child if child.parent != currentnode
         self.deduplicate(root)
 
+        # count the businesses for all the nodes
+        self.count_businesses(root)
+
         return root
 
     def setparent(self, node, parent):
         """
-        do inorder traversal to set the parent; 
+        do inorder traversal to set the parent;
         all child nodes should have correct parent before
         the current node has a parent
         """
@@ -104,6 +109,34 @@ class CategoryTreeCreator(object):
         node.children = [ child for child in node.children if child.parent==node]
         for child in node.children:
             self.deduplicate(child)
+
+    def count_businesses(self, root):
+        stmt = """SELECT
+                    c.id, count(cb.business_id) AS business_count
+                  FROM categories AS c
+                  LEFT JOIN categories_businesses AS cb
+                    ON (cb.category_id = c.id)
+                  GROUP BY c.id;"""
+        rows = self.query_helper.run_query(stmt)
+        business_counts = {r[0]: r[1] for r in rows}
+
+        # traverse tree to update categories
+        categoryStack = []
+        node = root
+        while node is not None:
+            try:
+                node.business_count = business_counts[node.category_id]
+            except KeyError:
+                if node is root:
+                    pass
+                else:
+                    print "Error: unable to find count for {}".format(node)
+
+            categoryStack.extend(node.children)
+            try:
+                node = categoryStack.pop()
+            except IndexError:
+                node = None
 
 class QueryHelper(object):
 
@@ -122,6 +155,17 @@ class QueryHelper(object):
     def commit(self):
         self.conn.commit()
 
+def enumerate_category_tree(root):
+    categories = [("Category", "Parent", "Business Count")]
+    categoryQueue = deque([(root, None)])
+
+    while len(categoryQueue) != 0:
+        node, parent_title = categoryQueue.popleft()
+        categories.append((node.title, parent_title, node.business_count))
+        [categoryQueue.append((child, node.title)) for child in node.children]
+
+    return categories
+
 if __name__=='__main__':
     import argparse
     parser = argparse.ArgumentParser()
@@ -136,4 +180,5 @@ if __name__=='__main__':
     root = tree_creator.create_category_tree()
     helper.close()
 
-    json.dump(root.children, args.output, cls=CategoryTreeEncoder)
+    # json.dump(root.children, args.output, cls=CategoryTreeEncoder)
+    json.dump(enumerate_category_tree(root), args.output, indent=2, separators=(',', ': '))
